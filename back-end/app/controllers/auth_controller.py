a5c1ba3fe9cfd19537122b6174c4dcd4fcc9dee2
sum812 <sssum812@gmail.com>
2023-11-20 15:17:56 +0700

    #1 define authen functions

---
+++ back-end/app/controllers/auth_controller.py
@@ -0,0 +1,56 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from ..settings.config import settings
+from ..connectDB import database
+from ..schemas import token_schemas
+from ..models import user_model
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
+
+def hash(password: str):
+    return pwd_context.hash(password)
+
+
+def verify(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = token_schemas.TokenData(id=str(id))
+    except JWTError:
+        raise credentials_exception
+    return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(user_model.Users).filter(
+        user_model.Users.id == token.id).first()
+    return user
