770dab77b8c6e26c0fa3c1296e8f1490532effd0
sum812 <sssum812@gmail.com>
2023-11-20 16:06:18 +0700

    auth function"

---
+++ front-end/src/services/AuthService.jsx
@@ -0,0 +1,28 @@
+import React, { } from "react";
+
+import {
+    Navigate,
+    useLocation
+} from "react-router-dom";
+
+export const setToken = (token) => {
+    // set token in localStorage
+    localStorage.setItem('tronghieuxinhdep', token)
+}
+
+export const fetchToken = (token) => {
+    // fetch the token
+    return localStorage.getItem('tronghieuxinhdep')
+}
+
+export function RequireToken({ children }) {
+
+    let auth = fetchToken()
+    let location = useLocation();
+
+    if (!auth) {
+        return <Navigate to="/" state={{ from: location }} />;
+    }
+
+    return children;
+}
