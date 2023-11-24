26828e774a82b51e8dda3692b6436fa883cf4f3e
Zan <minhthinh130202@gmail.com>
2023-11-24 09:38:24 +0700

    Change variable name to access token, add function check access token

--- front-end/src/services/AuthService.jsx
+++ front-end/src/services/AuthService.jsx
@@ -1,28 +1,51 @@
-import React, { } from "react";
-
-import {
-    Navigate,
-    useLocation
-} from "react-router-dom";
+import axios from "axios";
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
 
 export const setToken = (token) => {
-    // set token in localStorage
-    localStorage.setItem('tronghieuxinhdep', token)
+  localStorage.setItem("access_token", token);
+};
+
+export const fetchToken = () => {
+  return localStorage.getItem("access_token");
+};
+
+export function isAuth() {
+  const token = fetchToken();
+  if (!token) {
+    return false;
+  }
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  axios
+    .get("http://127.0.0.1:8000/auth/checkTokenExpired", config)
+    .then((response) => {
+      if (response.status === 200) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .catch(function (error) {
+      console.log(error, "error");
+      if (error.response && error.response.status === 404) {
+        alert("Invalid username or password. Please try again.");
+      } else {
+        alert("Login failed. Please try again.");
+      }
+    });
+  return false;
 }
 
-export const fetchToken = (token) => {
-    // fetch the token
-    return localStorage.getItem('tronghieuxinhdep')
-}
-
-export function RequireToken({ children }) {
-
-    let auth = fetchToken()
-    let location = useLocation();
-
-    if (!auth) {
-        return <Navigate to="/" state={{ from: location }} />;
-    }
-
-    return children;
+export function CheckToken({ children }) {
+  let auth = fetchToken();
+  let location = useLocation();
+  const navigate_login = <Navigate to="/" state={{ from: location }} />;
+  const navigate_current = children;
+  if (!auth) {
+    return navigate_login;
+  }
+  return navigate_current;
 }
