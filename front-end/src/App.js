4c49842b07a03278a70ce4262f74f32dcd00b137
Zan <minhthinh130202@gmail.com>
2023-11-30 15:54:38 +0700

    Add redux to check accesstoken

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,36 @@
 import './App.css';
-import { Navigate, createPath } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import DashBoard from './Components/DashBoard/DashBoard'
-import { CheckToken, isAuth } from './services/AuthService'
-import Login from './Pages/LoginPage/Login';
-import React, { } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { validateToken } from './services/AuthService'
+import Login from "./Pages/LoginPage/Login";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { validateToken } from './redux/actions/authAction';
 
 function App() {
+  const token = useSelector((state) => state.token);
+  const isLogged = useSelector((state) => state.auth.isLogged);
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    dispatch(validateToken())
+  }, [])
 
   return (
 
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-
         <Route
-          path="/dashboard"
+          path="/"
           element={
-            <CheckToken>
-              <DashBoard />
-            </CheckToken>
+            isLogged ? <DashBoard /> : <Navigate to="/login" />
           }
         />
+        <Route path="/login"
+          element={
+            isLogged ? <Navigate to="/" /> : <Login />
+          } />
 
       </Routes>
     </BrowserRouter>
