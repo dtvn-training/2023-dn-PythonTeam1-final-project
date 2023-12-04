69d1119b08c1df9d05a35fc642bb41201cc8392b
sum812 <sssum812@gmail.com>
2023-12-04 08:15:29 +0700

    complete user schemas

--- back-end/app/schemas/user_schemas.py
+++ back-end/app/schemas/user_schemas.py
@@ -1,23 +1,30 @@
 from pydantic import BaseModel, EmailStr
-from datetime import datetime
-
-
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
+from typing import List
 
 
 class UserInfo(BaseModel):
     email: EmailStr
+    first_name: str
+    last_name: str
+    role_id: str
+    address: str
+    phone: str
+
+
+class ListUserOut(BaseModel):
+    list_acount: List[UserInfo]
 
 
 class UserCreate(UserInfo):
     password: str
+    confirm_password: str
 
 
-class UserLogin(UserInfo):
+class UserLogin(BaseModel):
+    email: EmailStr
     password: str
+
+
+class UserUpdate(UserInfo):
+    password: str
+    confirm_password: str
