73766c25c6b165902356c2e9debe62823ce3d491
sum812 <sssum812@gmail.com>
2023-12-04 08:16:16 +0700

    complete create account, update account, get account

--- back-end/app/routers/user_router.py
+++ back-end/app/routers/user_router.py
@@ -1,45 +1,37 @@
-from fastapi import APIRouter, status, HTTPException, Depends, Header
+from fastapi import APIRouter, status, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
-from typing import Annotated
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Annotated
 
 from ..models import user_model
 from ..schemas import user_schemas
 from ..connectDB.database import get_db
-from ..controllers import auth_controller as auth
+from ..controllers import user_controller
 
 router = APIRouter(
-    prefix="/users",
-    tags=['Users']
+    prefix="/api/account",
+    tags=['Account']
 )
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@router.get("/", response_model=List[user_schemas.UserOut])
-async def get_users(db: Session = Depends(get_db)):
-    users = db.query(user_schemas.User).all()
-    return users
+@router.get("/", response_model=user_schemas.ListUserOut)
+async def get_accounts(db: Session = Depends(get_db)):
+    list_acount = user_controller.get_users(db)
+    return {"list_acount": list_acount}
 
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
-async def create_user(user: user_schemas.UserCreate, db: Session = Depends(get_db)):
-    # hash pwd
-    hashed_password = auth.hash(user.password)
-    user.password = hashed_password
-    new_user = user_model.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+async def create_an_account(user: user_schemas.UserCreate, db: Session = Depends(get_db)):
+    user_controller.create_a_user(user, db)
+    return {
+        "message": "Account successfully created"
+    }
 
 
-@router.get("/{id}")
-async def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(user_model.User).filter(
-        user_model.User.user_id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with id: {id} dose not exist!")
-
-    return user
+@router.put("/update", status_code=status.HTTP_200_OK)
+async def update_account(user_info: user_schemas.UserUpdate, db: Session = Depends(get_db)):
+    user_controller.update_user(user_info, db)
+    return {
+        "message": "Account successfully updated"
+    }
