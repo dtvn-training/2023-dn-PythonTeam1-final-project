ec65baea0a4b26282796952334b79d26871bec07
sum812 <sssum812@gmail.com>
2023-12-05 16:45:58 +0700

    fix config header

--- front-end/src/const/buildAPI.js
+++ front-end/src/const/buildAPI.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 // config
-const buildAPI = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
-    headers: { Authorization: `Bearer ${localStorage.getItem("access_token")}` }
-})
-
-
+const buildAPI =
+    axios.create({
+        baseURL: 'http://127.0.0.1:8000/'
+    })
+buildAPI.interceptors.request.use(config => (
+    {
+        ...config,
+        headers: { Authorization: `Bearer ${window.localStorage.getItem("access_token")}` }
+    }
+)
+)
 
 export default buildAPI;
