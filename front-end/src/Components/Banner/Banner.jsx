3e88233c76a9b1cc6059a3a6f03127ecbbf654d3
Zan <minhthinh130202@gmail.com>
2023-12-19 16:51:12 +0700

    Fix banner slider

--- front-end/src/Components/Banner/Banner.jsx
+++ front-end/src/Components/Banner/Banner.jsx
@@ -1,62 +1,71 @@
-import buildAPI from '../../const/buildAPI';
-import './Banner.scss'
+import buildAPI from "../../const/buildAPI";
+import "./Banner.scss";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 const Banner = () => {
-    const [index, setIndex] = useState(0);
-    const [images, setImages] = useState([]);
-    const [fetchDataTrigger, setFetchDataTrigger] = useState(true);  
-    const isMounted = useRef(false);
+  const [index, setIndex] = useState(0);
+  const [images, setImages] = useState([]);
+  const [fetchDataTrigger, setFetchDataTrigger] = useState(true);
+  const isMounted = useRef(false);
 
-    const nextSlide = useCallback(() => {
-        setIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-    }, [images.length]);
-
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await buildAPI.get("/api/banner/get_banner");
-                const list_banner = response.data.list_banner;
-                const imgPreviews = list_banner.map(banner => banner.img_preview);
-                if (imgPreviews.length !== 2) {
-                    console.log("Error fetching data");
-                    return;
-                }
-                setImages(imgPreviews);
-                setFetchDataTrigger(false);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        if (isMounted.current && fetchDataTrigger) {
-            fetchUserData();
-        } else {
-            isMounted.current = true;
-        }
-    }, [isMounted, fetchDataTrigger]);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            nextSlide();
-        }, 3000);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, [index, nextSlide]);
-
-    useEffect(() => {
-        setFetchDataTrigger(true);
-    }, [index]);
-
-    return (
-        <div className="slider">
-            {images.length === 2 && (
-                <img className='imageSlider' src={images[index]} alt="" />
-            )}
-        </div>
+  const nextSlide = useCallback(() => {
+    setIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
+  }, [images.length]);
+
+  const fetchUserData = () => {
+    buildAPI
+      .get("/api/banner/get_banner")
+      .then((response) => {
+        const list_banner = response.data.list_banner;
+        console.log(list_banner);
+        const imgPreviews = list_banner.map((banner) => banner.img_preview);
+        if (imgPreviews.length !== 2) {
+          console.log("Error fetching data");
+          return;
+        }
+        setImages(imgPreviews);
+        setFetchDataTrigger(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  useEffect(() => {
+      fetchUserData();
+  }, []);
+
+  useEffect(() => {
+    if (isMounted.current && fetchDataTrigger) {
+      fetchUserData();
+    } else {
+      isMounted.current = true;
+    }
+  }, [isMounted, fetchDataTrigger]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [index, nextSlide]);
+
+  useEffect(() => {
+    setFetchDataTrigger(true);
+  }, [index]);
+
+  return (
+    <div className="slider">
+      {images.length === 2 && (
+        <img className="imageSlider" src={images[index]} alt="" />
+      )}
+    </div>
+  );
 };
 
 export default Banner;
